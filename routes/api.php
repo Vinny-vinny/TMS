889941637d98f09c761092745877993ce699d34d
vinnyvinn <kituyiv@gmail.com>
2025-03-29 18:03:08 +0300

    initial commit

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\OrdersController;
-use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\TasksController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
@@ -21,8 +19,6 @@ Route::post('login',[AuthController::class,'login']);
 
 Route::middleware('auth:sanctum')->group( function () {
     Route::post('logout', [AuthController::class, 'logout']);
-    Route::apiResource('tasks',OrdersController::class);
-    Route::get('products',[ProductsController::class,'getProducts']);
     Route::get('user',[UsersController::class,'user']);
     Route::apiResource('task',TasksController::class);
 });
